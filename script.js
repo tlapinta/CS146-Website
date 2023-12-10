72109cedca7daf2b8e5bd7165b6a7e4a2324e8e8
Thomas LaPinta <tlapinta2002@gmail.com>
2023-12-10 11:56:14 -0500

    Adjusted image sizes in game for tablet and mobile devices

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function drag(event, imageUrl) {
 }
 
 
+
 function drop(event) {
   event.preventDefault();
   const container = document.getElementById('target-container');
@@ -21,38 +22,132 @@ function drop(event) {
 }
 
 function transformImage(image) {
+  var mediaQueries = [
+    window.matchMedia('(max-width: 480px)'),
+    window.matchMedia('(min-width: 481px) and (max-width: 768px)'),
+    window.matchMedia('(min-width: 769px)'),
+  ];
+
   if(image.src.includes("images/WheatBreadImage.png")){
     image.src = 'images/wheatSlice.png';
-    image.style.width = "300px";
+    if (mediaQueries[2].matches){
+      image.style.width = "300px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "200px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "85px"
+    }
   }else if(image.src.includes("images/AvocadoImage.png")){
     image.src = 'images/AvocadoSlice.png';
-    image.style.width = "200px";
+    if (mediaQueries[2].matches){
+      image.style.width = "200px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "150px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }else if(image.src.includes("images/LettuceImage.png")){
     image.src = 'images/LettuceSlice.png';
-    image.style.width = "300px";
+    if (mediaQueries[2].matches){
+      image.style.width = "300px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "200px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "75px"
+    }
   }else if(image.src.includes("images/grilledChicken.jpeg")){
     image.src = 'images/grilledChickenSlice.png';
+    if (mediaQueries[1].matches){
+      image.style.width = "150px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "65px"
+    }
   }else if(image.src.includes("images/SirachaMayoImage.png")){
     image.src = 'images/sirachaMayoSand.png';
-    image.style.width = "200px";
+    if (mediaQueries[2].matches){
+      image.style.width = "200px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "125px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }else if(image.src.includes("images/TomatoImage.png")){
     image.src = 'images/tomato_slice.png';
+    if (mediaQueries[1].matches){
+      image.style.width = "125px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }else if(image.src.includes("images/RyeBreadImage.png")){
     image.src = 'images/ryeBreadSlice.png';
-    image.style.width = "400px";
+    if (mediaQueries[2].matches){
+      image.style.width = "400px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "250px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "100px"
+    }
   }else if(image.src.includes("images/HamImage.png")){
     image.src = 'images/HamImage.png';
-    image.style.width = "200px";
+    if (mediaQueries[2].matches){
+      image.style.width = "200px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "125px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }else if(image.src.includes("images/MayoImage.png")){
     image.src = 'images/mayoSauce.png';
-    image.style.width = "300px";
+    if (mediaQueries[2].matches){
+      image.style.width = "300px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "200px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "85px"
+    }
   }else if(image.src.includes("images/BaconImage.png")){
     image.src = 'images/BaconImage.png';
-    image.style.width = "300px";
+    if (mediaQueries[2].matches){
+      image.style.width = "300px";
+    }
+    if (mediaQueries[1].matches){
+      image.style.width = "200px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "85px"
+    }
   }else if(image.src.includes("images/SwissCheeseImage.png")){
     image.src = 'images/swissCheeseSlice.png';
+    if (mediaQueries[1].matches){
+      image.style.width = "150px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }else if(image.src.includes("images/AmericanCheeseImage.png")){
     image.src = 'images/americanCheeseSlice.png';
+    if (mediaQueries[1].matches){
+      image.style.width = "150px"
+    }
+    if (mediaQueries[0].matches){
+      image.style.width = "55px"
+    }
   }
 }
 
